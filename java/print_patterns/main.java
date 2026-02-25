c38d256a76910518d711dc052372e3dd22f19268
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-02-25 16:19:08 +0530

    add: java - print pattern

---
+++ java/print_patterns/main.java
@@ -0,0 +1,22 @@
+package practicl01;
+import java.util.Scanner;
+
+public class print_patterns {
+
+	public static void printPattern(int n) {
+		int i, j;
+		for (i = 1; i <= n; i++) {
+			for (j = n; j >= n-i; j--) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.print("Enter a number :");
+		Scanner s = new Scanner(System.in);
+		int num = s.nextInt();
+		printPattern(num);
+	}
+}
